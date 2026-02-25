ebb9c2a3340970fe327a250bd79858b2de1195e1
Yeong Kim <litsync1@gmail.com>
2026-02-25 17:24:46 +0900

    영 작업 삭제

--- GitTest/src/com/Ex01.java
+++ GitTest/src/com/Ex01.java
@@ -4,7 +4,18 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 
-		System.out.println("영 개인 브랜치 커밋");
+		System.out.println("팀장 커밋");
+		System.out.println("팀원1 김영 두번째 커밋");
+		System.out.println("팀원2 김민경 세번째 커밋");
+		System.out.println("팀원3 고보경 네번째 커밋");
+		System.out.println("팀장 두번째 커밋 확인하세요!!");
+		System.out.println("팀원1 김영 커밋 확인~");
+		System.out.println("팀원2 민경 두번째 커밋 확인");
+		System.out.println("팀원3 보경 커밋 확인3");
+		System.out.println("팀장 세번째 커밋 및 출력문 정리");
+		
+	
+		
 	}
 
 }
