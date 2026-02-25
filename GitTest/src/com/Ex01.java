5d979cb6cb28d601bee9d608b810e8c50429d187
Yeong Kim <litsync1@gmail.com>
2026-02-25 16:39:03 +0900

    영 개인 브랜치 작업

--- GitTest/src/com/Ex01.java
+++ GitTest/src/com/Ex01.java
@@ -4,14 +4,7 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 
-		System.out.println("팀장 커밋");
-		System.out.println("팀원1 두번째 커밋");
-		System.out.println("팀원2 세번째 커밋");
-		System.out.println("팀원3 네번째 커밋");
-		System.out.println("팀장 두번째 커밋 확인하세요!!");
-		System.out.println("두번째 커밋 확인~");
-		System.out.println("두번째 커밋 확인2 ");
-		System.out.println("팀원 3 보경 커밋 확인3");
+		System.out.println("영 개인 브랜치 커밋");
 	}
 
 }
