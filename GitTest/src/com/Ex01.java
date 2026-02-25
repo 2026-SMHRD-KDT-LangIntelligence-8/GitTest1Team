431c178396251f7fe568aa4ab90c06eca499fc37
ryuseungho22 <poure22@gmail.com>
2026-02-25 16:00:54 +0900

    세번째 출력문 정리

--- GitTest/src/com/Ex01.java
+++ GitTest/src/com/Ex01.java
@@ -4,13 +4,14 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		System.out.println("팀장 커밋");
-		System.out.println("팀원1 두번째 커밋");
-		System.out.println("팀원2 세번째 커밋");
-		System.out.println("팀원3 네번째 커밋");
+		System.out.println("팀원1 김영 두번째 커밋");
+		System.out.println("팀원2 김민경 세번째 커밋");
+		System.out.println("팀원3 고보경 네번째 커밋");
 		System.out.println("팀장 두번째 커밋 확인하세요!!");
-		System.out.println("두번째 커밋 확인~");
-		System.out.println("팀원2 민경 두 번째 커밋 확인");
-		System.out.println("팀원 3 보경 커밋 확인3");
+		System.out.println("팀원1 김영 커밋 확인~");
+		System.out.println("팀원2 민경 두번째 커밋 확인");
+		System.out.println("팀원3 보경 커밋 확인3");
+		System.out.println("팀장 세번째 커밋 및 출력문 정리");
 	}
 
 }
